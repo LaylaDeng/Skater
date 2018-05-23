d67a6fcd07fef6698853a44f17195eb0efcf8f7f
Pramit Choudhary <pramitchoudhary@gmail.com>
2018-05-23 11:18:31 -0700

    syntatic correction in specifying and opertator

--- skater/core/local_interpretation/dnni/initializer.py
+++ skater/core/local_interpretation/dnni/initializer.py
@@ -31,8 +31,9 @@ class Initializer(object):
         feed_dict[self.input_tensor] = samples
         return self.session.run(output_tensor, feed_dict)
 
+    
     def _validate_baseline(self):
-        if self.baseline is not None & self.baseline.shape != ((1,) + self.samples.shape[1:]):
+        if self.baseline is not None and self.baseline.shape != ((1,) + self.samples.shape[1:]):
             if self.baseline.shape == self.samples.shape[1:]:
                 self.baseline = np.expand_dims(self.baseline, 0)
             else:
